5ee3bfcda514b6a30d73f4ba121d835f02457b19
Aza Maulana <azamaulanaaa@gmail.com>
2022-06-26 00:04:32 +0700

    test: simple test for default downloader
    this simple test is to make sure that it able to download given the
    right url.

---
+++ src/tests/test_default.py
@@ -0,0 +1,16 @@
+import unittest
+from pyunduh import default
+
+class Testdefault(unittest.TestCase):
+    def test_Default(self):
+        pairs = [
+            (["https://raw.githubusercontent.com/azamaulanaaa/dotfiles/main/neovim/setup.sh"], '#!/bin/bas'),
+        ]
+
+        for pair in pairs:
+            res = default.Default(*pair[0])
+            content = res.read(10).decode("utf-8")
+            self.assertEqual(content, pair[1])
+
+
+

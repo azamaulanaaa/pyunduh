d74c1ebd658327dab776f407117b00b282032004
Aza Maulana <azamaulanaaa@gmail.com>
2022-07-01 20:33:23 +0700

    fix: unencoded url

--- src/pyunduh/default.py
+++ src/pyunduh/default.py
@@ -1,4 +1,5 @@
 from urllib import request
+from urllib.parse import quote
 
 def Default(url: str, method: str = "GET"):
     params = getParams(url, method)
@@ -8,9 +9,12 @@ def Default(url: str, method: str = "GET"):
 
 def getParams(url: str, method: str) -> dict:
     return {
-        "url": url,
+        "url": urlEncoder(url),
         "method": method,
         "headers": {
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:101.0) Gecko/20100101 Firefox/101.0",
         },
     }
+
+def urlEncoder(url: str) -> str:
+    return quote(url, safe=":/")

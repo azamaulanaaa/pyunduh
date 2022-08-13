9dd14901d57b477c41a69858787ece6d8130cc34
Aza Maulana <azamaulanaaa@gmail.com>
2022-08-13 22:31:56 +0700

    fix: missing dependencies package for zippyshare

--- setup.py
+++ setup.py
@@ -9,6 +9,10 @@ setuptools.setup(
     python_requires = '>=3.7, <4',
     packages = setuptools.find_packages("src"),
     package_dir = {"": "src"},
+    install_requires = [
+        "bs4>='0.0.1'",
+        "js2py>='0.71'",
+    ],
     extras_require = {
         "dev": [
             "pytest",
